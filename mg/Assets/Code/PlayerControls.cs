266746171ed6f6261e355683702c26cc1825464b
mariaoliv <mos7589@nyu.edu>
2022-04-14 18:52:17 -0400

    camera follows player

--- mg/Assets/Code/PlayerControls.cs
+++ mg/Assets/Code/PlayerControls.cs
@@ -5,30 +5,35 @@ using UnityEngine;
 public class PlayerControls : MonoBehaviour
 {
     Rigidbody2D rb;
-    public int speed = 2;
+    public int speed = 7;
     public int jumpForce = 100;
+    public int fallSpeed = -5;
     public LayerMask ground;
-    public Transform feet;   
+    public Transform feet; 
+    public Vector2 startpos;
     bool grounded = false; 
     float groundCheckDist = 0.3f;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        startpos = transform.position;
         //rb.velocity = new Vector2(transform.localScale.x * speed, 0);
     }
 
     // Update is called once per frame
     void Update()
     {
-        rb.velocity = new Vector2(transform.localScale.x * speed, -2);
+        rb.velocity = new Vector2(transform.localScale.x * speed, fallSpeed);
         //if not grounded and not jumping, add down force
         if (grounded) {
             if (Input.touchCount > 0) {
                 rb.AddForce(new Vector2(0, jumpForce));
             }
         }
-        
+        if (transform.position.y < -10) {
+            transform.position = startpos;
+        }
     }
 
     void FixedUpdate()
